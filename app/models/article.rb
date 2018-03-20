d10491d6e09423ddaf530130b7c78ccf05cc61ad
Alfredo Re <alfredoreduarte@gmail.com>
2018-03-19 22:58:42 -0300

    Added class method to simulate articles being created in separated dates

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,16 @@ class Article < ApplicationRecord
 	has_and_belongs_to_many :users
 	# before_save :sanitize_json
 
+	def self.simulate_dates
+		today = DateTime.now
+		Article.order(id: :desc).all.each do |art|
+			art.created_at = today
+			art.updated_at = today
+			today = today - 1.day
+			art.save
+		end
+	end
+
 	private
 		# def sanitize_json
 			# self.content = JSON.parse(self.content)
