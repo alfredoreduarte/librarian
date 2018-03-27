f1a3a0742eb33dd115d88f8223ecddcd5bdc59f5
Alfredo Re <alfredoreduarte@gmail.com>
2018-03-27 11:38:23 -0400

    Added number of unsubscribed users to view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 		@projects_count = Project.count		
 		@users = User.first(10)
 		@users_count = User.count
+		@unsubscribed_count = User.unsubscribed.count
 		@articles = Article.first(10)
 		@articles_count = Article.count
 	end
